9f382f80662ca1eeebe102191d6eee48ac4741b6
= <=>
2025-05-19 10:21:10 -0500

    remodelacion de portafolio 1

--- scripts.js
+++ scripts.js
@@ -1,45 +1,20 @@
 window.addEventListener("load", ()=> {
-    const game = document.getElementById("game");
-    const startGame = document.getElementById("btnGame");
-    const scroll = document.getElementById("scroll");
-    const items = document.getElementsByClassName("item");
-    const enlaces = [];
-    let itemSelected = document.querySelector(".active");
-    let revisar = true;
+    const contactButton = document.getElementById("contactButton");
+    const modal = document.getElementById("modal");
+    const closeModal = document.getElementById("closeModal");
+    const openOptions = document.getElementById("openOptions");
+    const options = document.getElementById("options");
 
 
-    for (let i = 0; i < items.length; i++) {
-        const item = items[i];
-        const to = item.getAttribute("to");
-        const enlace = document.getElementById(to);
-        
-        if (enlace) {
-            const y = enlace.getBoundingClientRect().y - 200;
-            enlaces.push({y, item})
-    
-            item.addEventListener("click", ()=> {
-                revisar = false;
-                itemSelected.classList.remove("active");
-                item.classList.add("active");
-                itemSelected = item;
-                scroll.scrollTop = y;
-                revisar = true;
-            })
-        }
+    openOptions.onclick = ()=> {
+        options.classList.toggle("active");
     }
 
-    scroll.onscroll = ()=> {
-        if (revisar) {
-            for (let i = 0; i < enlaces.length; i++) {
-                const element = enlaces[i];
-                let y = element.y;
-                if (scroll.scrollTop >= y) {
-                    itemSelected.classList.remove("active");
-                    element.item.classList.add("active");
-                    itemSelected = element.item;
-                }
-            }
-        }
-    }
-    
+    contactButton.addEventListener("click", () => {
+        modal.classList.add("active");
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.classList.remove("active");
+    });
 })
