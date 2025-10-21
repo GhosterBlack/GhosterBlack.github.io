4093f8660fb66a62b8f3cfca97cca7e5fcb85866
Ivan Torres <ivan.look3002@gmail.com>
2025-10-21 18:33:36 -0500

    finalizacion

--- boda/main.js
+++ boda/main.js
@@ -1,6 +1,71 @@
+let buscar = () => { }
 document.addEventListener('DOMContentLoaded', () => {
+    const invitados = [
+        "Anita", "Olguita", "Alba y Robert", "David, Yurany y Valeria", "Josue y Eloina",
+        "Augusto", "Yimmy, Maribel y Juan Esteban", "Familia Garboza", "Teresita", "Adriana",
+        "Maria Helena.", "Naryis", "Miguel y Pilar", "Victor y Fidelia", "Ronald",
+        "Lucho", "Fercho", "Flor", "Mauro y Mariluz", "Cristian y Angie", "Hector y Judith",
+        "Zully", "Fanny", "Claudia", "Henry, Claudia y Esteban",
+        "Marco", "Miguel y Gladiz", "Jhon y Yazmin", "Maye y Nahomi", "Jose y Yulieth",
+        "Diannys", "Familia Piñeros", "Guillermo y Magda", "Emiro y Esperanza", "Daniel y Patricia", "Lien y Euledis",
+        "Sofia", "Gustavo", "Amparo y Eugenio", "Sara", "Jesus", "Pilar",
+        "Hanny", "Jorge", "Ivan y Mariela", "Familia Casadessus", "Luna", "Samuel",
+        "Yeissimar", "Nicolas", "Familia Amador", "Papo y Karen", "Juan Pablo", "Manuel", "Cesar", "Aurora", "Lupe",
+        "Simon, Juliet y Jael", "Eduardo", "Maria", "Franklin", "Tere y Alvaro", "Juan Cobos", "Never y Leti",
+        "Jaime, Monica e Isabella", "Pedro y Clara", "Lucho y Pilar", "Esteban y Valentina",
+        "Ynes y Marielys", "Deyanira", "Rosalba", "Marisol", "Marina", "Hilda"
+    ];
+
+    const masDeUnoInvitados = {
+        2: ["Alba y Robert", "Josue y Eloina", "Miguel y Pilar", "Victor y Fidelia", "Mauro y Mariluz",
+            "Cristian y Angie", "Hector y Judith", "Miguel y Gladiz", "Jhon y Yasmin", "Maye y Nahomi",
+            "Jose y Yulieth", "Guillermo y Magda", "Emiro y Esperanza", "Daniel y Patricia", "Lien y Euledis",
+            "Amparo y Eugenio", "Ivan y Mariela", "Tere y Alvaro", "Never y Leti"
+        ],
+        3: ["David, Yurany y Valeria", "Yimmy, Maribel y Juan Esteban", "Simon, Juliet y Jael",
+            "Jaime, Monica e Isabella", "Henry, Claudia y Esteban"
+        ],
+        4: ["Familia Garboza", "Familia Piñeros", "Familia Casadessus"],
+        zoom: [
+            "Pedro y Clara", "Lucho y Pilar", "Esteban y Valentina",
+            "Ynes y Marielys", "Deyanira", "Rosalba", "Marisol", "Marina", "Hilda", "Familia Amador", "Jorge y Nubia",
+            "Franklin"
+        ]
+    }
+
+    buscar = (nombre) => {
+        const lowerNombre = nombre.toLowerCase();
+        return invitados.findIndex(invitado => invitado.toLowerCase().includes(lowerNombre));
+    }
     const play = document.getElementById("play");
     const pause = document.getElementById("pause");
+    const href = location.search.substring(1);
+    let isZoom = false;
+
+    if (href.includes("invitado")) {
+        const invitado = parseFloat(decodeURIComponent(href.split('=')[1].replace(/\+/g, ' ')));
+        let invitadoNombre = invitados[invitado];
+        if (invitado >= 0 && invitado < invitados.length) {
+            const cantidadSpan = document.getElementById("cantidad");
+            if (invitadoNombre.includes(" y ") || invitadoNombre.includes("Familia") || invitadoNombre.includes(" e ")) {
+                document.getElementById("ver").textContent = "verlos";
+            }
+            if (masDeUnoInvitados[2].includes(invitadoNombre)) {
+                cantidadSpan.innerHTML = "2";
+            }
+            if (masDeUnoInvitados[3].includes(invitadoNombre)) {
+                cantidadSpan.innerHTML = "3";
+            }
+            if (masDeUnoInvitados[4].includes(invitadoNombre)) {
+                cantidadSpan.innerHTML = "4";
+            }
+            if (masDeUnoInvitados.zoom.includes(invitadoNombre)) {
+                document.getElementById("validez").style.display = "none";
+                isZoom = true;
+            }
+            document.getElementById("invitado").textContent = `, ${invitadoNombre}!`;
+        }
+    }
     /**
      * @type {HTMLAudioElement}
      */
@@ -42,13 +107,13 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("seconds").textContent = seconds;
     };
 
-    play.onclick = ()=> {
+    play.onclick = () => {
         pause.classList.remove("hidden");
         play.classList.add("hidden");
         audio.play();
     }
 
-    pause.onclick = ()=> {
+    pause.onclick = () => {
         play.classList.remove("hidden");
         pause.classList.add("hidden");
         audio.pause();
